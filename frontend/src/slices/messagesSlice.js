a371354b77550669b23657abedb33dbc3a7b08ad
vlad1slove1 <vlad5114@gmail.com>
2023-06-07 14:30:02 +0300

    update with extraReducers (load messages)

--- frontend/src/slices/messagesSlice.js
+++ frontend/src/slices/messagesSlice.js
@@ -3,6 +3,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 import { deleteChannel } from './channelsSlice.js';
+import fetchData from '../actions/fetchData.js';
 
 const initialState = {
   messages: [],
@@ -22,6 +23,19 @@ const messagesSlice = createSlice({
         const filteredMessages = state.messages
           .filter((message) => message.channelId !== action.payload.id);
         state.messages = filteredMessages;
+      })
+      .addCase(fetchData.pending, (state) => {
+        state.loadingStatus = 'loading';
+        state.error = null;
+      })
+      .addCase(fetchData.fulfilled, (state, action) => {
+        state.messages = action.payload.messages;
+        state.loadingStatus = 'idle';
+        state.error = null;
+      })
+      .addCase(fetchData, (state, action) => {
+        state.loadingStatus = 'failed';
+        state.error = action.error;
       });
   },
 });
